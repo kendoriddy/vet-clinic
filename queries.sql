c4941f01b1278907e9c3a9497133d34d097d74ce
Kehinde Onifade <onifkay@gmail.com>
2022-10-18 16:46:21 +0100

    Add screenshots for other transactions and add my queries to queries.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,25 @@ SELECT name, escape_attempt FROM animal WHERE weight_kg > 10.5;
 SELECT * FROM animal WHERE neutered is true;
 SELECT * FROM animal WHERE name != 'Gabumon';
 SELECT * FROM animal WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animal SET species = 'unspecified';
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+SELECT * FROM animals;
+UPDATE animals SET species = 'Pokemon' WHERE species is NULL;
+COMMIT;
+SELECT * FROM animals;
+BEGIN
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT S1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO S1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
