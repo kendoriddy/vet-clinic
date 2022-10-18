db17f73d4ee771854ba3e424c87a47ab5afbac59
Kehinde Onifade <onifkay@gmail.com>
2022-10-18 16:06:34 +0100

    Add screenshot for a transaction

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals(name, date_of_birth, escape_attempt, neutered, weight_kg)
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Agumon', date '2020-02-03', '0', 'true', '10.23'),
 ('Gabumon', date '2018-11-15', '2', 'true', '8'),
 ('Pikachu', date '2021-07-01', '1', 'false', '15.04'),
