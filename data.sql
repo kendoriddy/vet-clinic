804677ba2614f13cf198b6feac2155df23aa0ff5
Kehinde Onifade <onifkay@gmail.com>
2022-10-19 20:01:59 +0100

    Add data into owners table

--- data.sql
+++ data.sql
@@ -12,3 +12,13 @@ VALUES('Agumon', date '2020-02-03', '0', 'true', '10.23'),
 ('Boarmon', date '2005-06-07', '7', 'true', '20.4'),
 ('Blossom', date '1998-10-13', '3', 'true', '17'),
 ('Ditto', date '2022-05-14', '4', 'true', '22');
+
+-- Query Multiple Tables
+
+INSERT INTO owners(full_name, age)
+VALUES('Sam Smith', '34'),
+('Jennifer Orwell', '19'),
+('Bob', '45'),
+('Melody Pond', '77'),
+('Dean Winchester', '14'),
+('Jodie Whittaker', '38');
